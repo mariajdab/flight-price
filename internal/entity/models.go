0ec090e7b63682f5e4347898f8d61a9598cc5314
mariajdab <mariajdab@gmail.com>
2025-05-03 23:08:21 -0500

    update models

--- internal/entity/models.go
+++ internal/entity/models.go
@@ -3,24 +3,53 @@ package entity
 import "time"
 
 type Flight struct {
-	Airline      string
-	FlightNumber string
-	Origin       string
-	Destination  string
-	Departure    time.Time
-	Arrival      time.Time
-	Price        float64
-	Currency     string
-}
-
-type SearchReq struct {
+	Airline     string
 	Origin      string
 	Destination string
-	Date        time.Time
+	Departure   time.Time
+	Arrival     time.Time
+	Price       float64
+}
+
+type FlightSearchParam struct {
+	Origin        string
+	Destination   string
+	DateDeparture time.Time
+	Adults        int32
+	Currency      string
 }
 
 type FlightSearchResponse struct {
 	Provider string
+	Currency string
+	Cheapest Flight
+	Fastest  Flight
 	Flights  []Flight
 	Error    error
+}
+
+type FlightAmadeusResp struct {
+	Data []FlightOffer `json:"data"`
+}
+
+type FlightOffer struct {
+	Itineraries []struct {
+		Duration string `json:"duration"`
+		Segments []struct {
+			Departure struct {
+				At time.Time `json:"at"`
+			} `json:"departure"`
+			Arrival struct {
+				At time.Time `json:"at"`
+			} `json:"arrival"`
+			CarrierCode string `json:"carrierCode"`
+		} `json:"segments"`
+	} `json:"itineraries"`
+	Price struct {
+		Total    string `json:"total"`
+		Currency string `json:"currency"`
+	} `json:"price"`
+	Dictionaries []struct {
+		Carriers any `json:"carriers"`
+	} `json:"dictionaries"`
 }
