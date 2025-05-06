c804e3b8bd0ae805b2f02fb0832a4dab299bd3cf
mariajdab <mariajdab@gmail.com>
2025-05-05 21:28:34 -0500

    update search logic

--- src/internal/flights/service/search.go
+++ src/internal/flights/service/search.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/mariajdab/flight-price/internal/entity"
 	"github.com/mariajdab/flight-price/internal/providers"
+	"math"
 )
 
 type FlightService struct {
@@ -17,14 +18,48 @@ func NewFlightService(providers ...providers.Flight) *FlightService {
 	}
 }
 
-func (s *FlightService) SearchFlights(ctx context.Context, criteria entity.FlightSearchParam) []entity.FlightSearchResponse {
+func (s *FlightService) SearchFlights(ctx context.Context, criteria entity.FlightSearchParam) entity.FlightPriceResponse {
+	allCheapest := make([]entity.Flight, 0, len(s.providers))
+	allFastest := make([]entity.Flight, 0, len(s.providers))
+	allProviderFlights := make([]entity.FlightSearchResponse, 0, len(s.providers))
+
 	for _, provider := range s.providers {
-		flights, err := provider.SearchFlights(ctx, criteria)
+		resp, err := provider.SearchFlights(ctx, criteria)
 		if err != nil {
 			fmt.Println("en SearchFlights", err)
-			return []entity.FlightSearchResponse{}
+			return entity.FlightPriceResponse{}
 		}
-		fmt.Println(flights)
+
+		allCheapest = append(allCheapest, resp.Cheapest)
+		allFastest = append(allFastest, resp.Fastest)
+		allProviderFlights = append(allProviderFlights, resp)
 	}
-	return nil
+
+	cheapest := getGlobalCheapestOrFastestFlight(allCheapest, "cheapest")
+	fastest := getGlobalCheapestOrFastestFlight(allFastest, "fastest")
+
+	return entity.FlightPriceResponse{
+		Cheapest:         cheapest,
+		Fastest:          fastest,
+		FlightByProvider: allProviderFlights,
+	}
+}
+
+func getGlobalCheapestOrFastestFlight(flights []entity.Flight, criteria string) entity.Flight {
+	bestFlight := flights[0]
+	var fastestDuration = math.MaxInt64
+
+	for _, f := range flights {
+		if criteria == "cheapest" {
+			if f.Price < bestFlight.Price {
+				bestFlight = f
+			}
+		} else if criteria == "fastest" {
+			if f.DurationMinutes < fastestDuration {
+				bestFlight = f
+				fastestDuration = f.DurationMinutes
+			}
+		}
+	}
+	return bestFlight
 }
